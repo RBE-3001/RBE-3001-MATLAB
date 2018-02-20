21189c462ac19d6b7546b4b0566be0c5035a63ae
Hannah Baez <hrbaez@wpi.com>
2018-02-20 01:17:05 -0500

    TESTED add statics3001.m, needs implimentation and testing

---
+++ src/statics3001.m
@@ -0,0 +1,31 @@
+function Ftip = statics3001 (q, tq, d)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   test data   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%{
+%input joint angles
+q  = [0;
+      0;
+      5];
+
+%input joint torques
+tq = [1;
+      5;
+      0];
+
+%debug
+d = true;
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+DEBUG = d;
+
+%builds jacobian Matrix
+J = jacob0(q,d);
+Jp = J(1:3,:);
+
+%calculates force vector observed observed at tip
+Ftip = inv(Jp)*tq;
+
+end
