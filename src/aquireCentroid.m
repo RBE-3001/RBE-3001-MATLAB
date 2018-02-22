dcb706a34d9ec8e94439ceeb0ee32cd4fde6e57f
Hannah Baez <hrbaez@wpi.com>
2018-02-22 00:17:55 -0500

    TESTED add classifyColor.m, fixed aquireColor.m, unit tested both. Ready for integration

--- src/aquireCentroid.m
+++ src/aquireCentroid.m
@@ -3,7 +3,7 @@ function c = aquireCentroid(img, p, d)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   test data   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%{
+%
 close all; clear all; clc;
 %connects to webcam
 cam = webcam();
@@ -19,7 +19,7 @@ p = true;
 
 %debug
 d= true;
-%}
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %plot
@@ -33,36 +33,67 @@ centM = zeros(3,2);
 %initializes circle counter
 i = 1;
 
+if DEBUG && PLOT
+    figure;
+    imshow(img);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %blur image
 h = ones(5,5) / 25;
 img2 = imfilter(img, h);
-%figure;
-%imshow(img2);
+
+if DEBUG && PLOT
+    figure;
+    imshow(img2);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %filters out most of the junk
 img3 = createMask1(img2);
 img3 = imcomplement(img3);
-%figure;
-%imshow(img3);
+
+if DEBUG && PLOT
+    figure;
+    imshow(img3);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %remove tiny bits
 LB = 750;
 UB = 2200;
 img4 = xor(bwareaopen(img3, LB), bwareaopen(img3, UB));
-%figure;
-%imshow(img4);
+
+if DEBUG && PLOT
+    figure;
+    imshow(img4);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 [B,L] = bwboundaries(img4,'noholes');
 
 % Display the label matrix and draw each boundary
-imshow(label2rgb(L, @jet, [.5 .5 .5]))
-hold on
+if PLOT
+    imshow(label2rgb(L, @jet, [.5 .5 .5]));
+    hold on
+end
+
+%finds the boundaries
 for k = 1:length(B)
   boundary = B{k};
+  
+  if PLOT
   plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+  end
+  
 end
 
 stats = regionprops(L,'Area','Centroid');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 threshold = 0.60;
 
@@ -87,7 +118,7 @@ for k = 1:length(B)
 
   % mark objects above the threshold with a black circle
   if metric > threshold
-    centroid = stats(k).Centroid;
+    centroid = round(stats(k).Centroid);
     centM(i,:) = [centroid(1), centroid(2)];
     i = i + 1;
     
@@ -105,9 +136,10 @@ for k = 1:length(B)
     
   end
   
-  text(boundary(1,2)-35,boundary(1,1)+13,metric_string,'Color','y',...
-       'FontSize',12,'FontWeight','bold');
-  
+  if PLOT
+      text(boundary(1,2)-35,boundary(1,1)+13,metric_string,'Color','y','FontSize',12,'FontWeight','bold');
+ end
+ 
 end
 
 end
--- src/aquireColor.m
+++ src/aquireColor.m
@@ -1,9 +1,9 @@
-function color = aquireColor(img, c, p, d)
+function averageHue = aquireColor(img, c, p, d)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   test data   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%{
+%
 close all; clear all; clc;
 %connects to webcam
 cam = webcam();
@@ -14,12 +14,17 @@ pause(1)
 %grabs a frame of the webcame
 img = snapshot(cam);
 
+%test centroid
+ c = [253, 376;  % yellow
+      311, 273;  % green
+      404, 368]; % blue
+
 %plot
 p = true;
 
 %debug
-d= true;
-%}
+d = true;
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %plot
@@ -28,10 +33,63 @@ PLOT = p;
 %debug
 DEBUG = d;
 
-red = img(y, x, 1);
-green = img(y, x, 2);
-blue = img(y, x, 3);
+if DEBUG
+    figure;
+    imshow(img);
+end
 
-pixel = squeeze(img(x,y,:);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%grabs the color at the centroids
+%red = img(y, x, 1);
+%green = img(y, x, 2);
+%blue = img(y, x, 3);
+
+%pre-alocates the hue matrix, where each row is the average hue around a
+%centroid
+averageHue = zeros(size(c,1),1);
+
+%converts image from RGB format to HSV
+imgHSV = rgb2hsv(img);
+
+%sets radius of hue averaging
+radius = 75;
+    
+%loops through all of the centroid coordinates to grab the pixel HSV values
+for i = 1:size(c,1)
+    cX = c(i,1);
+    cY = c(i,2);
+    
+    %resets terms for calculating average hue repeatedly
+    count = 0;
+    sumH = 0;
+    
+    %adds up the hue from each pixel in the y-axis
+    for m = 0:radius*2
+        
+        %adds up the hue from each pixel in the x-axis
+        for n = 0:radius*2
+            x = cX - radius + m;
+            y = cY - radius + n;
+            hsv = squeeze(imgHSV(x,y,:)).';
+            sumH = sumH + hsv(1,1);
+            count = count +1;
+            
+            if DEBUG
+                disp(sprintf('x = %f, y = %f, hue = %f, sumH = %f, count = %f', x, y, hsv(1,1), sumH, count));
+            end
+        end
+                
+    end
+            
+    if DEBUG
+        disp(sprintf('Cx = %f, Cy = %f', cX,cY));
+        averageHue
+    end
+    
+    %averages the hue for each dot
+    averageHue(i,1) = sumH/count;
+
+end
 
 end
