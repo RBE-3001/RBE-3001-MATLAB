3c3014182fd02817e48a522c1c0bfa274fd24d78
Hannah Baez <hrbaez@wpi.com>
2018-02-25 16:33:11 -0500

    TESTED moved trajectory generation into a seperate function

--- src/main.m
+++ src/main.m
@@ -10,34 +10,19 @@ close all; clc; clear;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% variable declarations
 
-% enables/disables debug prints
-DEBUG   = false;
-
-%displays communication debug messages
-DEBUG_COMS = false;
-
-% enables/diables plotting
-PLOT    = true;
-
-% enables/disables data logging
-DATALOG = true;
-
-%calibrates the degrees per encoder tic
-degreesPerTics = 360/4096;
-
-%sets the lab number
-lab = 4;
-
-%sets axis parameters for live plot
-axe = [-400, 400, -400, 400, -150, 650];
+DEBUG   = false;                         %enables/disables debug prints
+DEBUG_COMS = false;                      %displays communication debug messages
+PLOT    = true;                          %enables/diables plotting
+DATALOG = true;                          %enables/disables data logging
+degreesPerTics = 360/4096;               %calibrates the degrees per encoder tic
+lab = 5;                                 %sets the lab number
+axe = [-400, 400, -400, 400, -150, 650]; %sets axis parameters for live plot
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% deletes old data logging data
-%original unchanged data from all nine channels
-delete armDataValuesCopy.csv;                 
-%has smoothed load cell values
-delete armDataValues.csv;
 
+delete armDataValuesCopy.csv; %original unchanged data from all nine channels
+delete armDataValues.csv;     %has smoothed load cell values
 delete X-Y-Z-Position.csv;
 delete X-Y-Z-Velocity.csv;
 delete X-Y-Z-Force.csv;
@@ -50,47 +35,38 @@ delete JointTorque.csv;
 delete averageLoadCell.csv;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% trajectory generation and conversion to encoder tics
+%% trajectory generation 
 
-%takes a maxix of X-Y-Z set-points and uses inverse kinematics to produce
-%a trajectory with variable data resolution
-%X-Y-Z set-points:
+%input points that will become a trajectory by using inverse kinematics to
+%produce a trajectory with variable data resolution
+desiredPoints = [ 233.85,   269.38,    247, 275.74, 230.93;  % X-axis poistion values
+                 -110.82,  -109.65,  9.572, 120.34, 117.58;  % Y-axis poistion values
+                  377.33,  -2.7074, 386.96, 6.1789, 372.87];  % Z-axis poistion values
 
-p = [ 233.85,   269.38,    247, 275.74, 230.93;  % X-axis poistion values
-     -110.82,  -109.65,  9.572, 120.34, 117.58;  % Y-axis poistion values
-      377.33,  -2.7074, 386.96, 6.1789, 372.87];  % Z-axis poistion values
+%selects between linear, cubic, and quintic polynomial point interpolation,
+%1 = linear, 3 = cubic, 5 = quintic
+interMode = 5;
 
-%P = [255;
-%      50;
-%     135];
+%sets the number of points between two points for tracjectory interpolation
+interPoints = 25;
 
-%{
-p = [355, 250;
-    0, 15;
-    135, 135];
-%}
-      
-% Cubic Polynomial interpolation between all setpoints
-P = cubicPoly(p, 25, 1, DEBUG);
+%sets the duration (s) between two points for cubic/quintic intermpolation
+nonLinearInterDuration = 3;
 
-% quintic Polynomial interpolation between all setpoints
-%P = quinticPoly(p, 10, 3, DEBUG);
-      
-% linear interpolation between all set-points
-%P = linearInterpolation(p, 1, DEBUG);
+%sets the number of points to be reapeated for better data resolution
+res = 1;
 
-% Can increase the number of identical points for greater data resolution when points are far apart.
-% Converts x-y-z points (mm) to encoder values
-viaPts = pointResolution(P, 1, degreesPerTics, DEBUG);
-
+encoderTrajectory = buildTrajectory( desiredPoints, interMode, interPoints, nonLinearInterDuration, res, degreesPerTics, DEBUG);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 %% commands movement
 
 tic %starts an elapse timer
 
 gripper = 1;
 
-[m, copym, time] = moveArm (viaPts, gripper, degreesPerTics, axe, lab, DATALOG, PLOT, DEBUG_COMS, DEBUG);
+[m, copym, time] = moveArm (encoderTrajectory, gripper, degreesPerTics, axe, lab, DATALOG, PLOT, DEBUG_COMS, DEBUG);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% data logging and post process plotting
@@ -106,12 +82,7 @@ if DATALOG
     
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% shutdown procedure
-
-% Clear up memory upon termination
-clear java;
-
+%displays end time of operation
 toc
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
